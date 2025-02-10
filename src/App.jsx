221e866edfa4da395678763c44dbd398b54c9126
FilippoBonanno <fbonanno15@gmail.com>
2025-02-10 19:14:31 +0100

    Rendering di liste, iterare su un array

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,61 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card.jsx'
 
-// LEZIONE N.9
+// LEZIONE N.10
 function App() {
   const [count, setCount] = useState(0)
   const imgUrl = "https://picsum.photos/seed/picsum/400/300"
+  const cards = [
+    {
+      id: 1,
+      title: "Tokyo",
+      description: "descrizione",
+      imgUrl: imgUrl,
+      isVisited: true,
+      children: "CHILDREN PROP",
+    },
+    {
+      id: 2,
+      title: "New York",
+      description: "descrizione",
+      imgUrl: imgUrl,
+      isVisited: false,
+      children: "CHILDREN PROP",
+    },
+    {
+      id: 3,
+      title: "Lisbona",
+      description: "descrizione",
+      imgUrl: imgUrl,
+      isVisited: true,
+      children: "CHILDREN PROP",
+    },
+    {
+      id: 4,
+      title: "Pechino",
+      description: "descrizione",
+      imgUrl: imgUrl,
+      isVisited: false,
+      children: "CHILDREN PROP",
+    },
+  ];
+
 
   return (
     <>
       <div className='grid grid-cols-4 gap-10'>
-        <Card title="Tokyo" description="descrizione" imgUrl={imgUrl} isVisited={true}>
-          CHILDREN PROP
-        </Card>
-        <Card title="Tokyo" description="descrizione" imgUrl={imgUrl} isVisited={false}>
-          CHILDREN PROP
-        </Card>
-        <Card title="Tokyo" description="descrizione" imgUrl={imgUrl} isVisited={true}>
-          CHILDREN PROP
-        </Card>
-        <Card title="Tokyo" description="descrizione" imgUrl={imgUrl} isVisited={false}>
-          CHILDREN PROP
-        </Card>
-
+        {/* METODO .MAP PER ITERARE SU UN ARRAY */}
+        {cards.map((city) => (
+          <Card
+            key={city.id}
+            title={city.title}
+            isVisited={city.isVisited}
+            imgUrl={city.imgUrl}
+            description={city.description}
+          >
+            {city.children}
+          </Card>
+        ))}
 
       </div >
 
